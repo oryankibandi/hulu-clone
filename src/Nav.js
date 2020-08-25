d5ccea9cbdf95e39839a1fb7591bd48553db5179
oryankibandi <shreckoryan@gmail.com>
2020-08-26 02:59:42 +0300

    completed the header and navbar

---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Nav.css';
+
+function Nav() {
+    return (
+        <div className="nav">
+            <h1>Trending</h1>
+            <h1>Top Rated</h1>
+            <h1>Action</h1>
+            <h1>Comedy</h1>
+            <h1>Horror</h1>
+            <h1>Romance</h1>
+            <h1>Mystery</h1>
+            <h1>sci-Fi</h1>
+            <h1>Western</h1>
+            <h1>Animation</h1>
+            <h1>Movie</h1>
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB4aRZ7QV54VEIVWnajmOh31X4fuqvqt4E",
+    authDomain: "hulu-clone-b8acf.firebaseapp.com",
+    databaseURL: "https://hulu-clone-b8acf.firebaseio.com",
+    projectId: "hulu-clone-b8acf",
+    storageBucket: "hulu-clone-b8acf.appspot.com",
+    messagingSenderId: "772108436493",
+    appId: "1:772108436493:web:0fbdc1042c8d66c1ed8144",
+    measurementId: "G-T857G6JVY6"
+  };
